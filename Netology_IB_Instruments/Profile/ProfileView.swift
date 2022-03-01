b634b3c6dbf7fdd85e790f55d3248dc4eae8b5fc
Cube033 <cube033@me.com>
2022-03-01 21:13:01 +0700

    main commit

---
+++ Netology_IB_Instruments/Profile/ProfileView.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Дмитрий on 23.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+ 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var birthDateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descrTextView: UITextView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let xibView = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)!.first as! UIView
+        addSubview(mainView)
+        xibView.frame = self.bounds
+        xibView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.addSubview(xibView)
+    }
+    
+    
+}
---
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Дмитрий on 01.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
